afd7fc7dac3bf5ab71b03a90993df357cbaae9a3
Nicole George <hello@nicolegeorge.com.au>
2023-02-07 11:55:29 +1030

    new components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,11 @@
-const App = () => {
-  return <div>hello</div>;
+import ListHeader from './components/ListHeader';
+
+const App = ({ listName }) => {
+  return (
+    <div className='app'>
+      <ListHeader listName={'🏖️ My Holiday Checklist'} />
+    </div>
+  );
 };
 
 export default App;
---
+++ client/src/components/ListHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ListHeader = ({ listName }) => {
+  return <h1 className="list-header">{listName}</h1>;
+};
+
+export default ListHeader;
